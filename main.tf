5d748d6b42310225769f80018dc6ce7957590ee4
Vish0ruyzaki <vishwajeetsingh62061@gmail.com>
2025-05-15 12:01:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.96.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    profile = "default"
+}
+
+resource "aws_instance" "nvirgina_ec2"{
+    ami = "ami-0f88e80871fd81e91"
+    instance_type = "t2.micro"
+    key_name = "docker"
+    
+    tags = {
+        Name = "virgina.ec2"
+    }
+}
+
+resource "aws_default_vpc" "default" {
+    tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Default subnet for us-east-1a"
+  }
+}
+
+resource "aws_vpc" "mainvpc" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.mainvpc.id
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
